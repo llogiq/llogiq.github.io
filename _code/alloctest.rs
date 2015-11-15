3d5bf20b77701b1cf7f184a7d7dbb6aed107814a
llogiq <bogusandre@gmail.com>
2015-11-15 21:30:38 +0100

    updated clippy talk

---
+++ _code/alloctest.rs
@@ -0,0 +1,5 @@
+use std::iter::Iterator;
+
+fn main() {
+	let x : Vec<usize> = (1..100000).collect();
+}
---
+++ _code/memprof2.rs
@@ -0,0 +1,17 @@
+extern {
+	//TODO je_print_stats() invocation
+}
+
+extern "C" {
+        
+}
+
+fn safe_print_stats() {
+	unsafe { /* TODO */ }
+}
+
+fn main() {
+	print_stats();
+	let x : Vec<usize> = (1.100000).collect();
+	print_stats();
+}
